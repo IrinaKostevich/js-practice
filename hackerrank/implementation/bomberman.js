f12e646617d249609719235577027a95b711f8b7
Irina Kostevich <irinakostevich@Irinas-MacBook-Pro.local>
2019-07-30 15:09:40 +1000

    refactoring with class World

--- hackerrank/implementation/bomberman.js
+++ hackerrank/implementation/bomberman.js
@@ -1,104 +1,123 @@
-function initiateGridMatrix(gridString) {
-    return gridString.map(string => string.split(''));
+// utils
+function createArray(length, fn) {
+    return Array.from(new Array(length), fn);
 }
 
-function getCurrentBombs(grid) {
-    const currentBombs = [];
+class World {
+    constructor(matrix) {
+        this._matrix = matrix;
+        this._width = this._matrix[0].length;
+        this._height = this._matrix.length;
+    }
 
-    for (let row = 0; row < grid.length; row += 1) {
-        for (let column = 0; column < grid[row].length; column += 1) {
-            if (grid[row][column] === 'O') {
-                currentBombs.push([row, column]);
+    static createFromString(gridString) {
+        const matrix = gridString.map(string => string.split(''));
+        return new World(matrix);
+    }
+
+    get width() {
+        return this._width;
+    }
+
+    set width(value) {
+        this._width = value;
+    }
+
+    get height() {
+        return this._height;
+    }
+
+    set height(value) {
+        this._height = value;
+    }
+
+    *getCurrentBombs() {
+        for (let row = 0; row < this._matrix.length; row += 1) {
+            for (let column = 0; column < this._matrix[row].length; column += 1) {
+                if (this._matrix[row][column] === 'O') {
+                    yield [row, column];
+                }
             }
         }
     }
 
-    return currentBombs;
-}
+    fillWorldWithBombs() {
+        const createRow = () => createArray(this.width, () => 'O');
+        this._matrix = createArray(this.height, createRow);
+    }
 
-function fillGridWithBombs(grid) {
-    grid.forEach(row => row.fill('O'));
-}
+    getCellsToDestroy(bombsCoordinates) {
+        const cellsToDestroy = new Set();
 
-function mapHas(map, key) {
-    return map.has(JSON.stringify(key));
-}
+        for (const [row, column] of bombsCoordinates) {
+            cellsToDestroy.add(JSON.stringify([row, column]));
 
-function mapAddUnique(map, key) {
-    if (mapHas(map, key)) return;
+            // Bottom
+            if (row + 1 < this.height) {
+                cellsToDestroy.add(JSON.stringify([row + 1, column]));
 
-    map.set(JSON.stringify(key), 1);
-}
+            }
+            // Top
+            if (row - 1 >= 0) {
+                cellsToDestroy.add(JSON.stringify([row - 1, column]));
 
-function getCellsToDestroy(bombsCoordinates, gridRows, gridColumns) {
-    const cellsToDestroy = new Map();
+            }
+            // Right
+            if (column + 1 < this.width) {
+                cellsToDestroy.add(JSON.stringify([row, column + 1]));
+            }
+            // Left
+            if (column - 1 >= 0) {
+                cellsToDestroy.add(JSON.stringify([row, column - 1]));
 
-    for (const [row, column] of bombsCoordinates) {
-        mapAddUnique(cellsToDestroy, [row, column]);
-
-        // Bottom
-        if (row + 1 < gridRows) {
-            mapAddUnique(cellsToDestroy, [row + 1, column]);
+            }
         }
-        // Top
-        if (row - 1 >= 0) {
-            mapAddUnique(cellsToDestroy, [row - 1, column]);
-        }
-        // Right
-        if (column + 1 < gridColumns) {
-            mapAddUnique(cellsToDestroy, [row, column + 1]);
-        }
-        // Left
-        if (column - 1 >= 0) {
-            mapAddUnique(cellsToDestroy, [row, column - 1]);
+
+        return cellsToDestroy;
+    }
+
+    detonate(cellsToDestroy) {
+        for (const value of cellsToDestroy) {
+            const [row, column] = JSON.parse(value);
+            this._matrix[row][column] = '.';
         }
     }
 
-    return cellsToDestroy;
-}
-
-function detonate(grid, cellsToDestroy) {
-    for (const [row, column] of cellsToDestroy) {
-        grid[row][column] = '.';
+    toString() {
+        return this._matrix.map(row => row.join(''));
     }
 }
 
-// Main function
 function bomberMan(n, gridString) {
     // return initial gridString
     if (n === 0 || n === 1) {
         return gridString;
     }
 
-    const gridMatrix = initiateGridMatrix(gridString);
+    const world = World.createFromString(gridString);
 
-    // return grid full of bombs
+    // return world full of bombs
     if (n % 2 === 0) {
-        fillGridWithBombs(gridMatrix);
-        return gridMatrix.map(row => row.join(''));
+        world.fillWorldWithBombs();
+        return world.toString();
     }
 
     // calculate seconds to go
     let seconds = n % 4;
     if (seconds === 1) seconds += 4;
-
     let currentBombs = [];
-    const rows = gridMatrix.length;
-    const columns = gridMatrix[0].length;
 
     for (let i = 2; i <= seconds; i += 1) {
         if (i % 2 === 0) {
-            currentBombs = getCurrentBombs(gridMatrix);
-            fillGridWithBombs(gridMatrix);
+            currentBombs = [...world.getCurrentBombs()];
+            world.fillWorldWithBombs();
         } else {
-            const cellsToDestroy = getCellsToDestroy(currentBombs, rows, columns);
-            const cellsCoordinates = Array.from(cellsToDestroy.keys()).map(JSON.parse);
-
-            detonate(gridMatrix, cellsCoordinates);
+            const cellsToDestroy = world.getCellsToDestroy(currentBombs);
+            world.detonate(cellsToDestroy);
         }
     }
 
-    return gridMatrix.map(row => row.join(''));
+    return world.toString();
 }
 
 // Grouping based on pattern
