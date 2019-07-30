d950f8b96d9e8b3e44ba3c02becec36222689c8a
Irina Kostevich <irinakostevich@Irinas-MacBook-Pro.local>
2019-07-30 12:13:41 +1000

    all handlers moved into makeDraggable function

--- tasks/drag-and-drop/scripts/ui/draggable.js
+++ tasks/drag-and-drop/scripts/ui/draggable.js
@@ -1,35 +1,28 @@
 import { getPointerPosition, getOffset, adjustPositionWithOffset, moveElementToPosition} from './utils.js';
-import { withinScreen } from '../core/boundary-strategies/within-screen.js';
-
-let onMouseMoveHandler = null;
-
-function onMouseDown(element, boundaryStrategy, event) {
-    const pointerPosition = getPointerPosition(event);
-    const offset = getOffset(element, pointerPosition);
-
-    onMouseMoveHandler = function(event) {
-        onMouseMove(element, offset, boundaryStrategy, event);
-    };
-
-    document.body.addEventListener('mousemove', onMouseMoveHandler);
-    document.body.addEventListener('mouseup', onMouseUp);
-}
-
-function onMouseUp() {
-    document.body.removeEventListener('mousemove', onMouseMoveHandler);
-}
-
-function onMouseMove(element, offset, boundaryStrategy, event) {
-    const pointerPosition = getPointerPosition(event);
-    const adjustedPosition = adjustPositionWithOffset(pointerPosition, offset);
-
-    if (boundaryStrategy({ element, position: pointerPosition, captureOffset: offset })) {
-        moveElementToPosition(element, adjustedPosition);
-    }
-}
 
 export function makeDraggable(element, boundaryStrategy) {
-    element.addEventListener('mousedown', (event) => {
-        onMouseDown(element, boundaryStrategy, event);
-    });
+    element.addEventListener('mousedown', onMouseDown);
+
+    let offset = null;
+
+    function onMouseDown(event) {
+        const pointerPosition = getPointerPosition(event);
+        offset = getOffset(element, pointerPosition);
+    
+        document.body.addEventListener('mousemove', onMouseMove);
+        document.body.addEventListener('mouseup', onMouseUp);
+    }
+    
+    function onMouseUp() {
+        document.body.removeEventListener('mousemove', onMouseMove);
+    }
+    
+    function onMouseMove(event) {
+        const pointerPosition = getPointerPosition(event);
+        const adjustedPosition = adjustPositionWithOffset(pointerPosition, offset);
+    
+        if (boundaryStrategy({ element, position: pointerPosition, captureOffset: offset })) {
+            moveElementToPosition(element, adjustedPosition);
+        }
+    }
 }
